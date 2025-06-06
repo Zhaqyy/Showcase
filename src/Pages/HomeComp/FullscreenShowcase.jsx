e1dbcfe95e9bfb4d43942022582e40bdb6c71cd4
zhaqyy <zaqrashyy7@gmail.com>
2025-06-05 18:18:09 -0600

    - mobile drawer trigger overhaul - icon change -quick nav animation improved

--- src/Pages/HomeComp/FullscreenShowcase.jsx
+++ src/Pages/HomeComp/FullscreenShowcase.jsx
@@ -68,16 +68,17 @@ const FullscreenShowcase = ({ showcase, onClose, isMobile, allShowcases, current
     trigger.innerHTML = `
       <div class="sidebar-header">
      
-          <div class='drawer-header'>
-            <div class='drag-handle'></div>
-          </div>
-       
+          
+       <div>
       <span class='label'>TITLE</span>
         <h3>${showcase.title}</h3>
-        <span class='label'>TAGS</span>
-        <div class="tags">${tagsHTML}</div>
-      </div>
-    `;
+        </div>
+        <hr/>
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g fill="none" fill-rule="evenodd"><path d="m12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035q-.016-.005-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093q.019.005.029-.008l.004-.014l-.034-.614q-.005-.018-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z"/><path fill="rgb(241, 204, 186)" d="M7 13a2 2 0 0 1 1.995 1.85L9 15v3a2 2 0 0 1-1.85 1.995L7 20H4a2 2 0 0 1-1.995-1.85L2 18v-3a2 2 0 0 1 1.85-1.995L4 13zm9 4a1 1 0 0 1 .117 1.993L16 19h-4a1 1 0 0 1-.117-1.993L12 17zm-9-2H4v3h3zm13-2a1 1 0 1 1 0 2h-8a1 1 0 1 1 0-2zM7 3a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2zm9 4a1 1 0 0 1 .117 1.993L16 9h-4a1 1 0 0 1-.117-1.993L12 7zM7 5H4v3h3zm13-2a1 1 0 0 1 .117 1.993L20 5h-8a1 1 0 0 1-.117-1.993L12 3z"/></g></svg>
+        </div>
+        `;
+    // <span class='label'>TAGS</span>
+    // <div class="tags">${tagsHTML}</div>
 
     trigger.addEventListener("click", toggleDrawer);
 
@@ -240,62 +241,62 @@ const FullscreenShowcase = ({ showcase, onClose, isMobile, allShowcases, current
     };
   }, [isMobile, drawerOpen]);
 
-// Navigation handlers
-const handleNext = useCallback(() => {
-  if (!allShowcases) return;
-  
-  const animateTransition = () => {
-    // Animation timeline
-    const tl = gsap.timeline();
-    
-    // Fade out current showcase
-    tl.to(contentContainerRef.current, { 
-      opacity: 0, 
-      duration: 0.2, 
-      delay: 0.6, // for menu to close
-    })
-    .add(() => {
-      // Update to next showcase
-      const nextIndex = (currentIndex + 1) % allShowcases.length;
-      setSelectedShowcase(allShowcases[nextIndex]);
-      setCurrentIndex(nextIndex);
-      setShowQuickNav(false);
-    })
-    // Fade in new showcase
-    .to(contentContainerRef.current, { 
-      opacity: 1, 
-      duration: 0.2 
-    });
-  };
+  // Navigation handlers
+  const handleNext = useCallback(() => {
+    if (!allShowcases) return;
 
-  animateTransition();
-}, [allShowcases, currentIndex, setSelectedShowcase, setCurrentIndex, isMenuOpen]);
+    const animateTransition = () => {
+      // Animation timeline
+      const tl = gsap.timeline();
 
-const handlePrev = useCallback(() => {
-  if (!allShowcases) return;
-  
-  const animateTransition = () => {
-    const tl = gsap.timeline();
-    
-    tl.to(contentContainerRef.current, { 
-      opacity: 0, 
-      duration: 0.2, 
-      delay: 0.6, // for menu to close
-    })
-    .add(() => {
-      const prevIndex = (currentIndex - 1 + allShowcases.length) % allShowcases.length;
-      setSelectedShowcase(allShowcases[prevIndex]);
-      setCurrentIndex(prevIndex);
-      setShowQuickNav(false);
-    })
-    .to(contentContainerRef.current, { 
-      opacity: 1, 
-      duration: 0.2 
-    });
-  };
+      // Fade out current showcase
+      tl.to(contentContainerRef.current, {
+        opacity: 0,
+        duration: 0.2,
+        delay: 0.6, // for menu to close
+      })
+        .add(() => {
+          // Update to next showcase
+          const nextIndex = (currentIndex + 1) % allShowcases.length;
+          setSelectedShowcase(allShowcases[nextIndex]);
+          setCurrentIndex(nextIndex);
+          setShowQuickNav(false);
+        })
+        // Fade in new showcase
+        .to(contentContainerRef.current, {
+          opacity: 1,
+          duration: 0.2,
+        });
+    };
 
-  animateTransition();
-}, [allShowcases, currentIndex, setSelectedShowcase, setCurrentIndex, isMenuOpen]);
+    animateTransition();
+  }, [allShowcases, currentIndex, setSelectedShowcase, setCurrentIndex, isMenuOpen]);
+
+  const handlePrev = useCallback(() => {
+    if (!allShowcases) return;
+
+    const animateTransition = () => {
+      const tl = gsap.timeline();
+
+      tl.to(contentContainerRef.current, {
+        opacity: 0,
+        duration: 0.2,
+        delay: 0.6, // for menu to close
+      })
+        .add(() => {
+          const prevIndex = (currentIndex - 1 + allShowcases.length) % allShowcases.length;
+          setSelectedShowcase(allShowcases[prevIndex]);
+          setCurrentIndex(prevIndex);
+          setShowQuickNav(false);
+        })
+        .to(contentContainerRef.current, {
+          opacity: 1,
+          duration: 0.2,
+        });
+    };
+
+    animateTransition();
+  }, [allShowcases, currentIndex, setSelectedShowcase, setCurrentIndex, isMenuOpen]);
 
   const handleSelectShowcase = index => {
     setSelectedShowcase(allShowcases[index]);
@@ -467,7 +468,7 @@ const handlePrev = useCallback(() => {
         </button>
 
         <button className='quick-nav-button' onClick={toggleQuickNav} aria-label='Show all showcases'>
-          ○○○
+        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24"><path fill="none" stroke="rgb(41, 65, 34)" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M2 18c0-1.54 0-2.31.347-2.876c.194-.317.46-.583.777-.777C3.689 14 4.46 14 6 14s2.31 0 2.876.347c.317.194.583.46.777.777C10 15.689 10 16.46 10 18s0 2.31-.347 2.877c-.194.316-.46.582-.777.776C8.311 22 7.54 22 6 22s-2.31 0-2.876-.347a2.35 2.35 0 0 1-.777-.776C2 20.31 2 19.54 2 18m12 0c0-1.54 0-2.31.347-2.876c.194-.317.46-.583.777-.777C15.689 14 16.46 14 18 14s2.31 0 2.877.347c.316.194.582.46.776.777C22 15.689 22 16.46 22 18s0 2.31-.347 2.877a2.36 2.36 0 0 1-.776.776C20.31 22 19.54 22 18 22s-2.31 0-2.876-.347a2.35 2.35 0 0 1-.777-.776C14 20.31 14 19.54 14 18M2 6c0-1.54 0-2.31.347-2.876c.194-.317.46-.583.777-.777C3.689 2 4.46 2 6 2s2.31 0 2.876.347c.317.194.583.46.777.777C10 3.689 10 4.46 10 6s0 2.31-.347 2.876c-.194.317-.46.583-.777.777C8.311 10 7.54 10 6 10s-2.31 0-2.876-.347a2.35 2.35 0 0 1-.777-.777C2 8.311 2 7.54 2 6m12 0c0-1.54 0-2.31.347-2.876c.194-.317.46-.583.777-.777C15.689 2 16.46 2 18 2s2.31 0 2.877.347c.316.194.582.46.776.777C22 3.689 22 4.46 22 6s0 2.31-.347 2.876c-.194.317-.46.583-.776.777C20.31 10 19.54 10 18 10s-2.31 0-2.876-.347a2.35 2.35 0 0 1-.777-.777C14 8.311 14 7.54 14 6" color="rgb(41, 65, 34)"/></svg>
         </button>
 
         <button className='nav-button next' onClick={handleNext} aria-label='Next showcase'>
